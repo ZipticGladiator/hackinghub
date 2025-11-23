73376df08d4f59c03381474337122185f0a96a6f
“ZipticGladiator” <siya.gladile@gmail.com>
2025-11-23 08:42:28 +0200

    refactor: Externalize CSS and JavaScript into dedicated files and move image asset to `images` directory.

---
+++ script.js
@@ -0,0 +1,8 @@
+function toggleRules() {
+    var x = document.getElementById("tasks");
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    } else {
+        x.style.display = "block";
+    }
+}
